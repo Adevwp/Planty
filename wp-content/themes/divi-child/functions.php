2fae526039a5068979fee1a0ae533c2b227a8352
DevWp <aurelie.devwp@gmail.com>
2023-11-08 22:48:01 +0100

    child theme personnalisation

--- wp-content/themes/divi-child/functions.php
+++ wp-content/themes/divi-child/functions.php
@@ -1,4 +1,5 @@
 <?php
+add_action( 'wp_enqueue_scripts', 'dt_enqueue_styles' );
 function dt_enqueue_styles() {
     $parenthandle = 'divi-style'; 
     $theme = wp_get_theme();
@@ -6,9 +7,7 @@ function dt_enqueue_styles() {
         array(), // if the parent theme code has a dependency, copy it to here
         $theme->parent()->get('Version')
     );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( $parenthandle ),
-        $theme->get('Version') 
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', 
+        array(), filemtime(get_stylesheet_directory() . '/css/theme.css')
     );
 }
-add_action( 'wp_enqueue_scripts', 'dt_enqueue_styles' );
